61928bae69cd6c1d282afae9929f67ae8d6a0873
Nacho <polodehombre4@gmail.com>
2025-12-09 13:43:48 +0100

    avance en juego Math.rnadom

---
+++ juego.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class juego {
+    public static void main(String[] args) {
+        int NR = numerorandom();
+        Scanner r = new Scanner(System.in);
+        do{
+            int  = r.nextInt();
+            
+
+        }
+
+        
+      
+    }
+
+      public static int numerorandom(){
+       int numero = (int)(Math.random() * 50) + 1;
+       return numero;
+        }
+
+
+}
+
