eeba9d1ef7fc227485a1d69c7f457c7c846706d4
Nacho <polodehombre4@gmail.com>
2025-12-10 13:31:32 +0100

    pedir numero a usuario

--- juego.java
+++ juego.java
@@ -3,14 +3,9 @@ import java.util.Scanner;
 public class juego {
     public static void main(String[] args) {
         int NR = numerorandom();
-        Scanner r = new Scanner(System.in);
-        do{
-            int  = r.nextInt();
-            
+        int NU = pedirnumeroausuario();
 
-        }
 
-        
       
     }
 
@@ -18,6 +13,14 @@ public class juego {
        int numero = (int)(Math.random() * 50) + 1;
        return numero;
         }
+
+        
+      public static int pedirnumeroausuario(){
+        Scanner r = new Scanner(System.in);
+        System.out.println("Escriba un número:");
+        int numusu = r.nextInt();
+        return numusu;
+      }
 
 
 }
